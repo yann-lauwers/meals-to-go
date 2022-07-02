52fe6716da50dbc73d12650a731389d927ea817d
Yann Lauwers <yann@embie.be>
2022-07-02 14:09:36 +0200

    style: init main layout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar"
+import { SafeAreaView, StyleSheet, Text, View, Platform, StatusBar } from "react-native"
+import { spacing } from "./src/utils/spacing"
+
+const isAndroid = Platform.OS === "android"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchContainer}>
+          <Text>search</Text>
+        </View>
+        <View style={styles.listContainer}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginTop: isAndroid ? StatusBar.currentHeight : 0,
   },
-});
+  listContainer: { backgroundColor: "blue", flex: 1, padding: spacing.PADDING.base },
+  searchContainer: { backgroundColor: "green", padding: spacing.PADDING.base },
+})
